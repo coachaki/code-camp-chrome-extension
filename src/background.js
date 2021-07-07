7b8747aeca3c6c0149e7009def8bf7346253c55f
Akifumi Yamamoto <af.yamamoto@gmail.com>
2021-07-07 13:32:58 -0700

    adding functionalities to the getting started guide

--- src/background.js
+++ src/background.js
@@ -3,4 +3,9 @@ let color = '#3aa757';
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
+    chrome.contextMenus.create({
+        "id": "setBackgroundColor",
+        "title": "Set Background Color"
+    });
+    console.log('Adding item to context menu.')
 });
