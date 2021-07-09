fd1b4ce0cdc1fc5521c1fb1036f8b5bdc68c8d58
Akifumi Yamamoto <af.yamamoto@gmail.com>
2021-07-08 21:45:54 -0700

    got context menu click working. there's room for organizing code better, but it works.

--- src/background.js
+++ src/background.js
@@ -7,6 +7,23 @@ chrome.runtime.onInstalled.addListener(() => {
         "id": "setBackgroundColor",
         "title": "Set Background Color"
     });
-    console.log('Adding item to context menu.')
+    console.log('Adding item to context menu 2.')
 });
 
+chrome.contextMenus.onClicked.addListener(async () => {
+    console.log('clicked context menu item');
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+    });
+});
+
+
+// The body of this function will be executed as a content script inside the current page
+function setPageBackgroundColor() {
+    chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+    });
+}
