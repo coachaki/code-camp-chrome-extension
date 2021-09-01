f8c6ba8f1f54ca5d60c5db92d73253af1eff2318
Akifumi Yamamoto <af.yamamoto@gmail.com>
2021-09-01 13:36:46 -0700

    done w/ challenge 11

--- ch11/src/background.js
+++ ch11/src/background.js
@@ -5,38 +5,49 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
     chrome.contextMenus.create({
-        "id": "gettingStarted",
-        "title": "Getting Started"
+        id: "omniPubURL",
+        title: "Omni Published Page",
+        contexts: ['link']
     }, function () {
         // add the children items in the callback function since we only want to create these if the parent item creation succeeds.
-        chrome.contextMenus.create({ title: 'Set Background Color', id: 'setBackgroundColor', parentId: 'gettingStarted' }, function () {
-            console.log('setBackgroundColor item added.');
+        chrome.contextMenus.create({ contexts: ['link'], title: 'Published Page', id: 'omniPubURL1', parentId: 'omniPubURL' }, function () {
+            console.log('omniPubURL1 item added.');
         });
-        chrome.contextMenus.create({ title: 'Console Log Click Data', id: 'consoleLogClickData', parentId: 'gettingStarted' }, function () {
+        chrome.contextMenus.create({ contexts: ['all'], title: 'Console Log Click Data', id: 'consoleLogClickData', parentId: 'omniPubURL' }, function () {
             console.log('consoleLogClickData item added.');
         });
     });
 
-    console.log('Adding item to context menu 2.')
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 
-    if (info.menuItemId == 'setBackgroundColor') {
-        // do action 1
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: setPageBackgroundColor,
-        });
-
-    } else if (info.menuItemId == 'consoleLogClickData') {
+    if (info.menuItemId == 'consoleLogClickData') {
         console.log(info, tab);
+    } else if (info.menuItemId === 'omniPubURL1') {
+        const linkUrl = info.linkUrl;
+        if (linkUrl.match(/\/dispatch/g)) {
+            const stagingPath = linkUrl.slice(linkUrl.indexOf('%2F'));
+            const site = linkUrl.split(/\/+/)[5];
+            chrome.storage.local.set({ stagingPath: stagingPath, site: site }, function () {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    function: openPublishedFile
+                });
+            });
+        }
     }
 });
 
 // The body of this function will be executed as a content script inside the current page
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
+function openPublishedFile() {
+    chrome.storage.local.get(['site', 'stagingPath'], function (result) {
+        const site = result.site;
+        const stagingPath = result.stagingPath;
+        fetch(`/files/info?site=${site}&path=${stagingPath}`)
+            .then(res => res.json())
+            .then(data => {
+                window.open(data.info.http_path, '_blank');
+            });
     });
 }
